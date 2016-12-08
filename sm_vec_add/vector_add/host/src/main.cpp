572bff8614a883854430e2a05134c9f01b118cde
JamesHKO <hsukiang@gmail.com>
2016-12-08 13:59:31 -0500

    Update main.cpp
    first time edit

--- sm_vec_add/vector_add/host/src/main.cpp
+++ sm_vec_add/vector_add/host/src/main.cpp
@@ -1,3 +1,4 @@
+// Edit for the first time
 // Copyright (C) 2013-2014 Altera Corporation, San Jose, California, USA. All rights reserved. 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this 
 // software and associated documentation files (the "Software"), to deal in the Software 
